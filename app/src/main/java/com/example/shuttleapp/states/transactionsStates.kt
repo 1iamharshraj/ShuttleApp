7bdab795073fe30218c96919553512b713b45844
Harsh Raj A <Hraj20000@gmail.com>
2024-04-22 13:22:46 +0530

    added states

---
+++ app/src/main/java/com/example/shuttleapp/states/transactionsStates.kt
@@ -0,0 +1,8 @@
+package com.example.shuttleapp.states
+
+class transactionsStates {
+    var amount : Double = 0.0
+    var type : Boolean = false
+    var rfid_id :  String = ""
+    var rfid_used : Boolean = false
+}
---
+++ app/src/main/java/com/example/shuttleapp/states/user.kt
@@ -0,0 +1,8 @@
+package com.example.shuttleapp.states
+
+class user {
+    var regno: String = ""
+    var email: String = ""
+    var pass: String = ""
+    var rfid : String = ""
+}
